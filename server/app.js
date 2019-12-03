cf7320a955a16ba6ec73c20bacb75378517ab36c
CaptainObjective <marcinwos18@gmail.com>
2019-12-03 20:22:15 +0100

    reworked

--- server/app.js
+++ server/app.js
@@ -1,30 +1,20 @@
-const express = require ('express');
+require('dotenv').config();
+const express = require('express');
+
 const app = express();
 app.use(express.json());
-require('dotenv').config();
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
 
-
-
-
-
 app.get('/', (req, res) => {
-
-    res.send('Home')
-
+  res.send('Home');
 });
 
 app.get('/api/products', (req, res) => {
-
-    res.send('products')
-
+  res.send('products');
 });
 
 app.get('/api/customers', (req, res) => {
-
-    res.send('customers')
-
-}); 
-
- 
+  res.send('customers');
+});
