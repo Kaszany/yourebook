c2f2825d797434173d3ba18fb1d1f1bffca4e6d0
Marta <j.martakaminska@gmail.com>
2019-12-06 17:40:53 +0100

    auth.v2

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt');
 // const _ = require('lodash');
-const {User} = require('../models/user');
+const {User} = require('../models/User');
 const Joi = require('joi');
 // const mongoose = require('mongoose');
 const express = require('express');
