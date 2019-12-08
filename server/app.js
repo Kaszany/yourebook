984c5b01167e449e0ba53d0c59c71b99bc4570ee
Hanna <hanna.najbar@gmail.com>
2019-12-08 19:54:09 +0100

    rejestracja_01

---
+++ server/app.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+const express = require('express');
+// const books = require('./routes/book');
+const user = require('routes/users');
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+  .then(() => console.log('Connected'))
+  .catch(err => console.error('Cant connect' + err));
+
+  const app = express();
+
+app.use(express.json());
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+app.get('/', (req, res) => {
+  res.send('Home');
+});app.get('/api/products', (req, res) => {
+  res.send('products');
+});app.get('/api/customers', (req, res) => {
+  res.send('customers');
+});
+
+// Posting ebook
+app.use(books);
+
+
