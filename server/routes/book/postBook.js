7d5b59eefa7bbd139f662ac1ba0a2e8bc8788af8
Radosław <hasiuk9@gmail.com>
2019-12-11 20:56:56 +0100

    bettermerge

--- server/routes/book/postBook.js
+++ server/routes/book/postBook.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bookModel = require('../models/Book');
+const bookModel = require('./../../models/Book');
 const GridFsStorage = require('multer-gridfs-storage');
 const multer = require('multer');
 
