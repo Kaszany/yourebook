03d2a3d40f439469d143a05467eac1dfbb764de6
Marta <j.martakaminska@gmail.com>
2019-12-19 21:19:51 +0100

    front-auth.v1

--- client/src/Components/Login/index.jsx
+++ client/src/Components/Login/index.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
 import Auth from './Auth';
-// import Register from './Register';
 
 class LoginForm extends React.Component {
-    render() {
-        return (
-            // <div className="ui container" style={{marginTop:'50px'}}>
-            //     <Auth />
-            // </div>
-            <div className="ui container" style={{marginRight: '14px'}}>
-            <Auth />
-        </div>
-        )
-    }
+
+	render() {
+		return (
+			<div className="ui container" style={{marginTop: '50px'}}>
+				<Auth />
+			</div>
+		)
+	}
 };
 
 export default LoginForm;
-
-
--- client/src/Views/Login.jsx
+++ client/src/Views/Login.jsx
@@ -12,6 +12,4 @@ const Login = () => {
   )
 };
 
-
 export default Login;
-
