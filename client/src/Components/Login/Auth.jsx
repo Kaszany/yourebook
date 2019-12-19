e8937058792dc01ce962b00acb3bd0e299144940
CaptainObjective <marcinwos18@gmail.com>
2019-12-19 22:17:37 +0100

    działający fetch, ale możemy zostać przy axios

--- client/src/Components/Login/Auth.jsx
+++ client/src/Components/Login/Auth.jsx
@@ -3,58 +3,59 @@ import axios from 'axios';
 import { Form, Input, Button } from 'semantic-ui-react';
 
 class Auth extends Component {
-	state = { email: '', password: ''};
+  state = { email: '', password: '' };
 
-	formChange = e => {
+  formChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
 
-		const { name, value} = e.target;
-		this.setState({ [name]: value});
-	};
+  onFormSubmit = async e => {
+    e.preventDefault();
+    const { email, password } = this.state;
 
-	onFormSubmit = async e => {
-		e.preventDefault();
-		const { email, password } = this.state;
+    // axios
+    //   .post('/api/auth', {
+    //     email: email,
+    //     password: password,
+    //   })
+    //   .then(response => {
+    //     console.log(`response ${response}`);
+    //   })
+    //   .catch(error => {
+    //     console.log(`login error ${error}`);
+    //   });
 
-		axios
-			.post(
-				'/api/auth',
-				{
-					email: email,
-					password: password
-				}
-			)
-			.then(response => {
-				console.log(`response ${response}`);
-			})
-			.catch(error => {
-				console.log(`login error ${error}`);
-			})
+    const response = await fetch(`/api/auth`, {
+      method: 'post',
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    });
+    const date = await response.json();
+    console.log(date);
+  };
 
-		
-		// // const response = await fetch(`/api/auth?email=${email}&password=${password}'`, {
-			// method: 'POST'
-		// });
-		// const date = await response.json();
-		// console.log(date);
-	};
-
-	render() {
-		return (
-			<Form onSubmit={this.onFormSubmit}>
-				<Form.Field>
-					<label>Email</label>
-					<Input placeholder="Email" name="email" value={this.state.email} onChange={this.formChange}>
-					</Input>
-				</Form.Field>
-				<Form.Field>
-					<label>Password</label>
-					<Input placeholder="Password" name="password" value={this.state.password} onChange={this.formChange}>
-					</Input>
-				</Form.Field>
-					<Button type="submit">Login</Button>
-			</Form>
-		);
-	};
+  render() {
+    return (
+      <Form onSubmit={this.onFormSubmit}>
+        <Form.Field>
+          <label>Email</label>
+          <Input placeholder="Email" name="email" value={this.state.email} onChange={this.formChange}></Input>
+        </Form.Field>
+        <Form.Field>
+          <label>Password</label>
+          <Input placeholder="Password" name="password" value={this.state.password} onChange={this.formChange}></Input>
+        </Form.Field>
+        <Button type="submit">Login</Button>
+      </Form>
+    );
+  }
 }
 
 export default Auth;
