58cb7c0e4cee817912ec99a904dab774269c78bb
Hanna <hanna.najbar@gmail.com>
2019-12-21 13:32:34 +0100

    cleaning01

---
+++ client/src/Login/Register.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Form, Input, Button } from 'semantic-ui-react';
+
+
+class Auth extends Component {
+    state = { email: '', password: '' };
+
+
+
+
+}
