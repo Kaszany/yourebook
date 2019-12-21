ad7207c739b526e734c4da8b8762e02bdd5f0c03
Hanna <hanna.najbar@gmail.com>
2019-12-21 14:53:38 +0100

    cleaning03

--- client/src/Login/index.jsx
+++ client/src/Login/index.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import Auth from './Auth';
 
 class LoginForm extends React.Component {
-
-	render() {
-		return (
-			<div className="ui container" style={{marginTop: '50px'}}>
-				<Auth />
-			</div>
-		)
-	}
+    render() {
+        return (
+            <div className="ui container" style={{marginTop:'50px'}}>
+                <Auth />
+            </div>
+        )
+    }
 };
 
 export default LoginForm;
