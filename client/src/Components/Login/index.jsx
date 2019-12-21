c509cfa06343266a5f46c462aaf0c518597c9455
Hanna <hanna.najbar@gmail.com>
2019-12-21 16:34:13 +0100

    Form_only

---
+++ client/src/Components/Login/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+// import Auth from './Auth';
+import Register from './Register';
+
+class LoginForm extends React.Component {
+    render() {
+        return (
+            // <div className="ui container" style={{marginTop:'50px'}}>
+            //     <Auth />
+            // </div>
+            <div className="ui container" style={{marginTop:'50px'}}>
+            <Register />
+        </div>
+        )
+    }
+};
+
+export default LoginForm;
