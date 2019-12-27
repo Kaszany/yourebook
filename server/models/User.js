dfb8633d082c216ce1faf2257113bef9bf8033a3
Hanna <hanna.najbar@gmail.com>
2019-12-27 19:46:18 +0100

    passwordconf 01

--- server/models/User.js
+++ server/models/User.js
@@ -23,6 +23,12 @@ const userSchema = new mongoose.Schema({
 		minlength: 5,
 		maxlength: 1024
 	},
+	// passwordconf: {
+	// 	type: String,
+	// 	required: true,
+	// 	minlength: 5,
+	// 	maxlength: 1024
+	// },
 	isAdmin: Boolean
 });
 
@@ -38,7 +44,8 @@ function validateUser(user) {
 	const schema = {
 		name: Joi.string().min(5).max(50).required(),
 		email: Joi.string().min(5).max(255).required().email(),
-		password: Joi.string().min(5).max(255).required()
+		password: Joi.string().min(5).max(255).required(),
+		// passwordconf: Joi.string().min(5).max(255).required(),
 	};
 	return Joi.validate(user, schema);
 }
