15c33e5434cc263c31000a72e3438b4c0da6add4
Lukasz <kaszany@onet.eu>
2019-12-29 09:37:39 +0100

    Grom z jasnego nieba

--- client/src/Views/Home.jsx
+++ client/src/Views/Home.jsx
@@ -4,12 +4,15 @@ import AddBook from '../Components/AddBook';
 import BookElement from '../Components/BookElement';
 import { Card, Modal, Button  } from 'semantic-ui-react';
 
+
 class Home extends Component {
+
 
 state = { findBooks: [],
           allBooks: [],
           modalFindOpen: false,
           modalAllOpen: false,
+          modalOneOpen: false,
         };
     
 findData = findBooks => {
@@ -28,6 +31,10 @@ handleShowOpen = () => this.setState({modalAllOpen: true});
 
 handleShowClose = () => this.setState({ modalAllOpen: false});
 
+handleOneOpen = () => this.setState({ modalOneOpen: true});
+
+handleOneClose = () => this.setState({ modalOneOpen: false});
+
 render(){
   return (
     <>
@@ -38,16 +45,28 @@ render(){
         onClose={this.handleClose}
       >
         <Modal.Content>
-      {this.state.findBooks.map(book => {
-        return (
+        {this.state.findBooks.map( book => {
+        return (   
         <Card color="blue" key={book._id}>
         <Card.Content>
-          {"Title: " + book.title.toUpperCase() + "  *  Author: " + book.author.toUpperCase() + "  *  Year: " + book.year + "  *  Genre: " + book.genre.toUpperCase()}
+          {"Title: " + book.title.toUpperCase() + "  *  Author: " + book.author.toUpperCase() + "  *  Year: " + book.year + "  *  Genre: " + book.genre.toUpperCase()} 
+          <Modal.Actions>
+          <Button type="submit" color="blue" icon="redo" labelPosition="right" content="Show me this book" floated="right" onClick={this.handleOneOpen}/>
+          </Modal.Actions>
+          <Modal
+          size={'large'}
+          open={this.state.modalOneOpen}
+          onClose={this.handleOneClose}
+          > 
+          <Modal.Content>
+          {"Title: " + book.title.toUpperCase() + " ||| Author: " + book.author.toUpperCase() + " ||| Year: " + book.year + " ||| Genre: " + book.genre.toUpperCase() + " ||| Book Cover: " + book.bookCover + " ||| PDF: " + book.PDF + " ||| Mongo Book ID: " + book._id}
+          </Modal.Content>
+          </Modal>
         </Card.Content>       
-        </Card>
-      );
+        </Card> 
+      ); 
      })}
-       </Modal.Content>
+        </Modal.Content>
       </Modal>
       <BookElement showAllData={this.showAllData} handleShowOpen={this.handleShowOpen}/>
       <Modal
@@ -55,17 +74,29 @@ render(){
         open={this.state.modalAllOpen}
         onClose={this.handleShowClose}
       >
-        <Modal.Content>
+      <Modal.Content>
       {this.state.allBooks.map(book => {
       return (
         <Card color="yellow" key={book._id}>
         <Card.Content>
-            {"Title: " + book.title.toUpperCase() + " * Author: " + book.author.toUpperCase() + " * Year: " + book.year + " * Genre: " + book.genre.toUpperCase()} 
+          {"Title: " + book.title.toUpperCase() + " * Author: " + book.author.toUpperCase() + " * Year: " + book.year + " * Genre: " + book.genre.toUpperCase()} 
+          <Modal.Actions>
+          <Button type="submit" color="yellow" icon="redo" labelPosition="right" content="Show me this book" floated="right" onClick={this.handleOneOpen}/>
+          </Modal.Actions>
+          <Modal
+          size={'large'}
+          open={this.state.modalOneOpen}
+          onClose={this.handleOneClose}
+          > 
+          <Modal.Content>
+          {"Title: " + book.title.toUpperCase() + " ||| Author: " + book.author.toUpperCase() + " ||| Year: " + book.year + " ||| Genre: " + book.genre.toUpperCase() + " ||| Book Cover: " + book.bookCover + " ||| PDF: " + book.PDF + " ||| Mongo Book ID: " + book._id}
+          </Modal.Content>
+          </Modal>
         </Card.Content>       
         </Card>
       );
      })}
-           </Modal.Content>
+      </Modal.Content>
       </Modal>
       <AddBook />
     </>
