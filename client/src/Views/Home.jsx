969a61a00c58d01d9040dc6627cec0e7f1d49baa
Hanna <hanna.najbar@gmail.com>
2020-01-02 01:51:24 +0100

    home sketch 2

--- client/src/Views/Home.jsx
+++ client/src/Views/Home.jsx
@@ -4,26 +4,53 @@ import LoginForm from '../Components/Login'
 import RegisterForm from '../Components/Register'
 
 const Home = () => {
-  return (
-    <>
-    <div>
-      <div className="ui segment" style={{marginLeft: '55%', display: 'flex'}}>
-        <LoginForm />
-        <RegisterForm />
-      </div>
-      <div className="ui segment">
-      <img src='/client/public/bookstorm.jpg'/>
 
-      </div>
+  //modal
+  // return (
+  //   <>
+  //   <div>
+  //     <div style={{marginLeft: '70%', marginTop: '14px', display: 'flex', justifyItems: 'right'}}>
+  //       <LoginForm />
+  //       <RegisterForm />
+  //     </div>
+  //     <div className="ui segment">
+  //     <img src='/client/public/bookstorm.jpg'/>
+
+  //     </div>
     
     
-      <div className="ui segment">
-        <SearchForm />
-      </div>
+  //     <div className="ui segment">
+  //       <SearchForm />
+  //     </div>
       
-    </div>
-    </>
-  );
+  //   </div>
+  //   </>
+  // );
+
+    //table
+    return (
+      <>
+      <div>
+        <div style={{marginLeft: '55%', marginTop: '14px', display: 'flex', justifyItems: 'right'}}>
+          <LoginForm />
+          <RegisterForm />
+        </div>
+        <div className="ui segment">
+        <img src='/client/public/bookstorm.jpg'/>
+  
+        </div>
+      
+      
+        <div className="ui segment">
+          <SearchForm />
+        </div>
+        
+      </div>
+      </>
+    );
+
+
+
 };
 
 export default Home;
