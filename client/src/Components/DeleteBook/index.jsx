6d376196a1eec38b46c3233d7b7892f147204e30
Marta <j.martakaminska@gmail.com>
2020-01-06 15:34:20 +0100

    Revert "delete book from list"
    This reverts commit 4d1eaf8e20d099af13052fd6a8a5fb1745f3bec7.

--- client/src/Components/DeleteBook/index.jsx
+++ client/src/Components/DeleteBook/index.jsx
@@ -1,36 +1,39 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Button, Card, Modal, Header, Form, Transition } from 'semantic-ui-react';
+// // import { transitions, positions, Provider as AlertProvider } from 'react-alert';
+// import AlertTemplate from 'react-alert-template-basic';
+// import Alert from './Alert';
+
+// const options = {
+	// position: positions.BOTTOM_CENTER,
+	// timeout: 5000,
+	// offset: '30px',
+	// transition: transitions.SCALE
+// }
+
 
 class DeleteBook extends Component {
 	constructor() {
 		super();
 		this.state = {
 			modalOpen: false,
-			bookOnList: true
 		}
 	}
 	
 	handleOpen = () => this.setState({ modalOpen: true });
 	handleClose = () => this.setState({ modalOpen: false });
 
-	deleteBookFromList = () => {
-		this.setState({bookOnList: false})
-		console.log(this.props.book)
-
-	}
-
-	deleteBook = async e => {
+	deleteThisBook = async e => {
 		e.preventDefault();
 		const id = this.props.book._id;
 
 		axios
 			.delete(`/api/books/${id}`)
 			.then(response => {
-				this.deleteBookFromList()
-				// setTimeout(() => {
-					// window.location.reload()
-				// }, 1000);
+				setTimeout(() => {
+					window.location.reload()
+				}, 1000);
 					
 			})
 			.catch(error => {
@@ -59,7 +62,7 @@ class DeleteBook extends Component {
 			>
 				<Header>Are you sure you want to delete this book?</Header>
 				<Modal.Content>
-					<Form onSubmit={this.deleteBook}>
+					<Form onSubmit={this.deleteThisBook}>
 						<Modal.Actions onSubmit={this.handleClose}>
 							<Button 
 							style={{ marginLeft: '0px' }}
@@ -75,7 +78,6 @@ class DeleteBook extends Component {
 								  color="red"
 								  content="Delete book" 
 								  floated="right"
-								  onClick={this.handleClose}
 								/>
 								}
 							>
